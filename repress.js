f8d8c7f6fcee005b259f691c4fec414e1baf1f1c
zyue <1640837234@qq.com>
2022-03-23 21:53:32 +0800

    tool

---
+++ repress.js
@@ -0,0 +1,12 @@
+let express=require("express");
+let app = express();
+let port=3000;
+
+app.use(express.static('public'));
+//编辑路由
+app.get("/",(req,res)=>{
+    res.send("hello")
+});
+app.listen(port ,()=>{
+    console.log("开启");
+});
