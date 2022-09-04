0f882ab5b57fa22a16920409fdd1a51011f2c5e8
DongRui <zefitwse@foxmail.com>
2022-09-04 12:09:57 +0800

    Revert "本地"
    This reverts commit a75bceb05070932c8a890cbc4db57a9c7cf9126b.

--- repress.js
+++ repress.js
@@ -8,7 +8,6 @@ app.get("/",(req,res)=>{
     res.send("hello","ddddddddddd")
     res.send("hello","333333")
     res.send("hello","本地")
-    res.send("hello","本地2")
     
 });
 app.listen(port ,()=>{
