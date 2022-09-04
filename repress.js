5395cdcb83dd99b4db329091d6d3402f19fefdd4
DongRui <zefitwse@foxmail.com>
2022-09-04 11:54:40 +0800

    commit: ddddddddd

--- repress.js
+++ repress.js
@@ -5,7 +5,7 @@ let port=3000;
 app.use(express.static('public'));
 //编辑路由
 app.get("/",(req,res)=>{
-    res.send("hello")
+    res.send("hello","ddddddddddd")
 });
 app.listen(port ,()=>{
     console.log("开启");
